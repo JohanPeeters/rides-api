2f8b6d88d57f451ba3c45396f10a57952082c777
Johan Peeters <yo@johanpeeters.com>
2018-12-24 09:50:17 +0100

    error message for failed update

--- ride_sharing/update.js
+++ ride_sharing/update.js
@@ -69,7 +69,7 @@ const dynamo = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'})
       if (err)
         result.statusCode = 403
         result.body = JSON.stringify({
-          message: err
+          message: 'users can only update their own rides'
         })
         callback(null, result)
   })
